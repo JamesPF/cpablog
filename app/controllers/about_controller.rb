77c29e480916657e661ba6b410f156142b8adc38
JamesPF <jamesfelz@gmail.com>
2015-02-06 14:56:56 -0800

    stylesheet change and add front end views

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,2 +1,7 @@
 class AboutController < ApplicationController
+
+  def show
+    @info = Bio.last
+  end
+
 end
--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,2 +1,7 @@
 class BlogController < ApplicationController
+
+  def index
+    @article = Post.all.order([:date]).reverse
+  end
+
 end

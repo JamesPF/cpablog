ab3c12444b6ca65832e5ba6fe132f93002d075a1
JamesPF <jamesfelz@gmail.com>
2015-02-17 18:03:35 -0800

    logout added to each admin page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Michaelfelzpc::Application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'settings', to: 'users#edit', as: 'settings' 
   get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users, :except => [:index]
   resources :sessions
